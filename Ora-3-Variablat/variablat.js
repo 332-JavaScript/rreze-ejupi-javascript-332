11051b372c765981d755a45e68d5cf321e9e7d66
Rreze <rrezeejupi@gmail.com>
2026-09-09 17:58:30 +0200

    Mesimi per variablat dhe tipet e te dhenave

---
+++ Ora-3-Variablat/variablat.js
@@ -0,0 +1,59 @@
+let emri = "Rreze";
+let mosha = 21;
+let gjinia = "Femer";
+
+console.log(emri, mosha, gjinia);
+
+let variabla = 500;
+const PI = 3.14;
+var variabla3 = 3;
+
+
+console.log(variabla, PI, variabla3);
+
+
+let test = 1;
+let Test = 2;
+let TEST = 3;
+
+console.log(test, Test, TEST);
+
+//emertime te variablave qe lejohen ne javascript
+let ngjyra = "pink";
+let _ngjyra = "pink";
+
+//emertime te variablave qe nuk lejohne ne js
+// let 1ngjyra = "pink";
+// let -ngjyra = "pink";
+// let !ngjyra = "pink";
+
+//deklarimi i variables
+
+let gjatesia;
+
+console.log(gjatesia);
+
+//inicializimi i variables
+
+gjatesia = 1.75;
+
+console.log(gjatesia);
+
+let gjeresia = 12;
+
+//Tipet e te dhenave
+
+let emri1 = "Rreze"; //String 
+let numri = 10; //Number
+let eshteStudent = true; //Boolean 
+let klasa; //Undefined
+let bosh = null; 
+let zero = 0;
+let bigNum = 987656567876545; //BigInt
+
+console.log(emri1, numri, eshteStudent, klasa, bosh, zero, bigNum);
+
+console.log(typeof emri1, typeof numri, typeof eshteStudent, typeof klasa, typeof bosh, typeof zero, typeof bigNum);
+
+
+
